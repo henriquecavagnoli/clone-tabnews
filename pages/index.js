0313a4eb1d084da1da7eeff44836fb6adf78edde
henriquecavagnoli <henrique_cavagnoli@hotmail.com>
2026-01-20 22:43:56 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mãe, eu amo você. Sou muito grato por todo seu apoio ao longo da minha vida! 😎</h1>
+  return (
+    <h1>
+      Mãe, eu amo você. Sou muito grato por todo seu apoio ao longo da minha
+      vida! 😎
+    </h1>
+  );
 }
 
 export default Home;
